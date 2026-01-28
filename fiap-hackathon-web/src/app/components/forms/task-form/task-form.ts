6cf39bfaac5cbe4417f403f0769f84e6504410ec
Daniel Guimarães <danguimaraes86@gmail.com>
2026-01-27 21:24:34 -0300

    web: refact: forms folder and task status and priorities to 'as const'

---
+++ fiap-hackathon-web/src/app/components/forms/task-form/task-form.ts
@@ -0,0 +1,107 @@
+import { Component, inject, Injectable } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { provideLuxonDateAdapter } from '@angular/material-luxon-adapter';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerIntl, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { MatSelectModule } from '@angular/material/select';
+import { DateTime } from 'luxon';
+import { Task, TASK_PRIORITIES, TASK_STATUSES, TaskPriority, TaskStatus } from '../../../models/task.models';
+
+@Injectable()
+class CustomDatepickerIntl extends MatDatepickerIntl {
+  override calendarLabel = 'Calendário';
+  override openCalendarLabel = 'Abrir calendário';
+  override closeCalendarLabel = 'Fechar calendário';
+  override prevMonthLabel = 'Mês anterior';
+  override nextMonthLabel = 'Próximo mês';
+  override prevYearLabel = 'Ano anterior';
+  override nextYearLabel = 'Próximo ano';
+  override prevMultiYearLabel = 'Anos anteriores';
+  override nextMultiYearLabel = 'Próximos anos';
+  override switchToMonthViewLabel = 'Visualização do mês';
+  override switchToMultiYearViewLabel = 'Escolher mês e ano';
+}
+
+interface TaskFormData {
+  title: FormControl<string>,
+  description?: FormControl<string | null>,
+  status: FormControl<TaskStatus>,
+  priority: FormControl<TaskPriority>,
+  dueDate?: FormControl<DateTime<boolean> | null>,
+  startDate?: FormControl<DateTime<boolean> | null>,
+}
+
+@Component({
+  selector: 'app-task-form',
+  imports: [
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinner,
+    MatSelectModule,
+    MatDatepickerModule
+  ],
+  providers: [
+    provideLuxonDateAdapter(),
+    { provide: MatDatepickerIntl, useClass: CustomDatepickerIntl }
+  ],
+  templateUrl: './task-form.html',
+  styleUrl: './task-form.scss',
+})
+export class TaskForm {
+  private _fb = inject(FormBuilder);
+  private _dialogRef = inject(MatDialogRef<TaskForm>);
+
+  taskForm: FormGroup<TaskFormData>;
+  isLoading = false;
+
+  statusList = Object.values(TASK_STATUSES);
+  priorityList = Object.values(TASK_PRIORITIES);
+
+  constructor() {
+    this.taskForm = this._fb.group<TaskFormData>({
+      title: this._fb.control('', {
+        nonNullable: true, validators: [Validators.required, Validators.maxLength(200)]
+      }),
+      description: this._fb.control(null),
+      status: this._fb.control(TASK_STATUSES.PENDING.value, {
+        nonNullable: true, validators: [Validators.required]
+      }),
+      priority: this._fb.control(TASK_PRIORITIES.MEDIUM.value, {
+        nonNullable: true, validators: [Validators.required]
+      }),
+      dueDate: this._fb.control<DateTime | null>(null),
+      startDate: this._fb.control<DateTime | null>(null),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.taskForm.valid) {
+      this.isLoading = true;
+      const formValue = this.taskForm.value;
+
+      const taskData: Partial<Task> = {
+        ...formValue,
+        description: formValue.description ?? undefined,
+        dueDate: formValue.dueDate?.toISODate() ?? undefined,
+        startDate: formValue.startDate?.toISODate() ?? undefined
+      };
+      console.log(taskData)
+
+      this._dialogRef.close(taskData);
+    }
+  }
+
+  onCancel(): void {
+    this._dialogRef.close();
+  }
+
+}
--- fiap-hackathon-web/src/app/models/task.models.ts
+++ fiap-hackathon-web/src/app/models/task.models.ts
@@ -21,5 +21,20 @@ export interface Task {
   completedAt?: string
 }
 
-export type TaskStatus = 'pending' | 'in_progress' | 'completed' | 'cancelled'
-export type TaskPriority = 'low' | 'medium' | 'high' | 'urgent'
+export const TASK_STATUSES = {
+  PENDING: { value: 'pending', label: 'Pendente' },
+  IN_PROGRESS: { value: 'in_progress', label: 'Em Progresso' },
+  COMPLETED: { value: 'completed', label: 'Concluída' },
+  CANCELLED: { value: 'cancelled', label: 'Cancelada' }
+} as const
+
+export type TaskStatus = typeof TASK_STATUSES[keyof typeof TASK_STATUSES]['value']
+
+export const TASK_PRIORITIES = {
+  LOW: { value: 'low', label: 'Baixa' },
+  MEDIUM: { value: 'medium', label: 'Média' },
+  HIGH: { value: 'high', label: 'Alta' },
+  URGENT: { value: 'urgent', label: 'Urgente' }
+} as const
+
+export type TaskPriority = typeof TASK_PRIORITIES[keyof typeof TASK_PRIORITIES]['value']
